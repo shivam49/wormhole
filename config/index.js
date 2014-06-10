fe4ba7c08f4740c7aa45e31c768af6b3c75b172f
Alexander Do <alex@eekoh.me>
2014-06-10 12:20:59 -0700

    Fix Config Defaults

--- config/index.js
+++ config/index.js
@@ -5,4 +5,4 @@ var path      = require('path');
 var defaults  = require(path.join(__dirname, 'config.json'));
 var config    = require(path.join(__dirname, process.env.NODE_ENV + '.json'));
 
-module.exports = _.defaults(defaults, config);
+module.exports = _.defaults(config, defaults);
