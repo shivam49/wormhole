88dbd3a698ece1696e87b6df221dc310015dbc98
patrick gorrell <patrick.gorrell@localeos.com>
2014-06-13 11:14:09 -0700

    Sytles for masonry

--- modules/masonry/index.js
+++ modules/masonry/index.js
@@ -16,12 +16,4 @@ $(function() {
     }
   });
   wall.fitWidth();
-
-  var temp = '<div class="brick {size}" style="border: 2px solid #fff; background-color: {color}"><div class="cover"></div></div>';
-  var size = "size23 size22 size21 size13 size12 size11".split(" ");
-  $(".add-more").click(function() {
-    var	html = temp.replace('{size}', size[size.length * Math.random() << 0])
-    .replace('{color}', colour[colour.length * Math.random() << 0]);
-    wall.prepend(html);
-  });
 });
