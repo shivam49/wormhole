78e21662f58153dbebfa1d1a912720bfc04463ff
Alexander Do <alex@eekoh.me>
2014-06-13 08:13:13 -0700

    Default to dev enviornment

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@
 var _         = require('lodash');
 var path      = require('path');
 var defaults  = require(path.join(__dirname, 'config.json'));
-var config    = require(path.join(__dirname, process.env.NODE_ENV + '.json'));
+var config    = require(path.join(__dirname, (process.env.NODE_ENV || 'dev') + '.json'));
 
 module.exports = _.defaults(config, defaults);
